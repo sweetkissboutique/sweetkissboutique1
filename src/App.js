9b28bd915a81b491540e40d663ca822a99b97ea2
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2021-08-22 19:07:13 -0500

    added the final build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './ak.png';
+import logo from './static/images/ak.png';
+import etsy from './static/images/etsy-icon.png';
+
 import './App.css';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import TextField from '@material-ui/core/TextField';
 import FormLabel from '@material-ui/core/FormLabel';
 import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+import { Avatar, Link } from '@material-ui/core';
 
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <br/>
-         <br/>
-         <br/>
-        <div>
-        <InstagramIcon style={{ fontSize: 80 }} />
-        <Typography>
-            Instagram
+        <Typography variant="h8" >
+          Say Hello to your new favorite!
         </Typography>
+        <br />
+        <br />
+        <Link href="https://www.etsy.com/shop/SweetKissBoutique" >
+          <img src={etsy} className="etsy-logo" alt="logo" />
+        </Link>
+        <Typography>
+          Etsy
+        </Typography>
+        <br />
+        <br />
+        <div>
+        <Link href="https://www.instagram.com/sweetkissboutique/" >
+          <InstagramIcon style={{ fontSize: 80 }} />
+          <Typography>
+            Instagram
+          </Typography>
+          </Link>
         </div>
       </header>
     </div>
